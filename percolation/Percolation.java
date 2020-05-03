f57857309f7d837f60b9bb974d2d121bd315518a
AndrewKozyrev <47481139+AndrewKozyrev@users.noreply.github.com>
2020-05-03 13:38:42 +0300

    Update Percolation.java

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -11,7 +11,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     private int[][] grid;
     private WeightedQuickUnionUF uf;
-    private int vtop;
+    private int vtop = 0;
     private int vbot;
 
     // creates n-by-n grid, with all sites initially blocked
@@ -22,15 +22,16 @@ public class Percolation {
         grid = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                grid[i][j] = n * i + j;
+                grid[i][j] = n * i + j + 1;
             }
         }
-        uf = new WeightedQuickUnionUF(n * n);
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        vbot = n * n + 1;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (row <= 0 || col <= 0 || row > grid[0].length || col > grid[0].length) {
+        if (!validate(row - 1, col - 1)) {
             throw new IllegalArgumentException("argument out of range")
         }
         int r = row - 1;
@@ -38,23 +39,23 @@ public class Percolation {
         if (!isOpen(r, c)) {
             grid[r][c] = 1;
             if (validate(r - 1, c) && isOpen(r - 1, c)) {
-                uf.union(grid[r][c], grid[r - 1][c]);
+                uf.union(index(r, c), index(r - 1, c));
             }
             if (validate(r + 1, c) && isOpen(r + 1, c)) {
-                uf.union(grid[r][c], grid[r + 1][c]);
+                uf.union(index(r, c), index(r + 1, c));
             }
             if (validate(r, c - 1) && isOpen(r, c - 1)) {
-                uf.union(grid[r][c], grid[r][c - 1]);
+                uf.union(index(r, c), index(r, c - 1));
             }
             if (validate(r, c + 1) && isOpen(r, c + 1)) {
-                uf.union(grid[r][c], grid[r][c + 1]);
+                uf.union(index(r, c), index(r, c + 1));
             }
         }
     }
 
     private boolean validate(int row, int col) {
         int N = grid[0].length;
-        if (row <= 0 || col <= 0 || row >= N || col >= N) {
+        if (row < 0 || col < 0 || row >= N || col >= N) {
             return false;
         }
         return true;
@@ -68,11 +69,18 @@ public class Percolation {
         return grid[row][col] == 1;
     }
 
+    private int index(int row, int col) {
+        if (validate(row, col)) {
+            return grid[0].length * (row - 1) + col;
+        }
+    }
+
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         if (row <= 0 || col <= 0 || row > grid[0].length || col > grid[0].length) {
             throw new IllegalArgumentException("argument out of range")
         }
+        return uf.connected(vtop, )
     }
 
     // returns the number of open sites

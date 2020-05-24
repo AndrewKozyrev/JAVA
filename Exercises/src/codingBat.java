d7720be69407f57bc193deca7a753c21339316de
AndrewKozyrev <andrey_kozyrev_001@mail.ru>
2020-05-24 17:08:02 +0300

    Create codingBat.java

---
+++ Exercises/src/codingBat.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class codingBat {
+    public static void main(String[] args){
+        int[] temp = {2, 5, 3, 7};
+        has23(temp);
+    }
+    public static void has23(int[] nums){
+        System.out.println();
+    }
+}

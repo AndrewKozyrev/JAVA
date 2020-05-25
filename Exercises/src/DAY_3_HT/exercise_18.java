80e1ba5969a32fdd7e3de8c6b6277e1e79e54b2d
AndrewKozyrev <andrey_kozyrev_001@mail.ru>
2020-05-25 15:49:59 +0300

    Внёс исправления

--- Exercises/src/DAY_3_HT/exercise_18.java
+++ Exercises/src/DAY_3_HT/exercise_18.java
@@ -1,19 +1,32 @@
 package DAY_3_HT;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class exercise_18 {
-    public static void main(String[] args){
-        System.out.println(divide(0, 0));
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Введите первое число: ");
+        String num1 = read.readLine();
+        System.out.print("Введите второе число: ");
+        String num2 = read.readLine();
+        System.out.println("Результат: " + divideIntegers(num1, num2));
     }
 
-    public static double divide(Object a, Object b) throws IllegalArgumentException {
-        if ( !Number.class.isInstance(a) || !Number.class.isInstance(b)  )
-            throw new IllegalArgumentException("Input numbers!");
+    public static double divideIntegers(String a, String b) throws IllegalArgumentException {
         try {
-                return ((Number) a ).doubleValue() / ((Number) b ).doubleValue();
-        } catch (ArithmeticException e) {
-            throw new ArithmeticException(e.toString());
+            int num1 = Integer.parseInt(a);
+            int num2 = Integer.parseInt(b);
+            if (num2 == 0) {throw new ArithmeticException();}
+            return num1 / (double) num2;
+        } catch (ArithmeticException e1) {
+            throw new ArithmeticException("Делить на ноль нельзя.");
+
+        } catch (NumberFormatException e2) {
+            String msg = "Вы пытаетесь разделить " + a + "\tна\t" + b + "\n" +
+                    "Хотя бы один из аргументов не является целым числом.\n";
+            throw new NumberFormatException(msg + e2.toString());
         }
 
     }

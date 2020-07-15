44ac3931c38ad7974b8bf94a45cf5ce1f08b1293
AndrewKozyrev <andrey_kozyrev_001@mail.ru>
2020-07-15 10:55:49 +0300

    Update

--- Exercises/src/codingBat.java
+++ Exercises/src/codingBat.java
@@ -1,12 +1,27 @@
-import java.util.Arrays;
-import java.util.List;
-
 public class codingBat {
-    public static void main(String[] args){
-        int[] temp = {2, 5, 3, 7};
-        has23(temp);
+    public static String deFront(String str) {
+        int n = str.length();
+        StringBuilder result = new StringBuilder();
+        if ( n >= 2 )
+        {
+            switch ( str.charAt(0) ) {
+                case 'a': result.append('a');
+                default: break;
+            }
+            switch ( str.charAt(1) ) {
+                case 'b': result.append('b');
+                default: break;
+            }
+            result.append( str.substring(2) );
+        }
+        else if ( n == 1 ) {
+            return str.equals("a") ? "a" : "";
+        }
+
+        return result.toString();
     }
-    public static void has23(int[] nums){
-        System.out.println();
+
+    public static void main(String[] args) {
+        deFront("Hello");
     }
 }

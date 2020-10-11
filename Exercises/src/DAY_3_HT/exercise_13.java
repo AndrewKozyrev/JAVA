94b11e9bae159a8012c488d00af062c88b3ba3df
AndrewKozyrev <andrei.kozireff2014@mail.ru>
2020-10-11 18:04:04 +0300

    удалил файлы, которые не относятся к проектам

--- Exercises/src/DAY_3_HT/exercise_13.java
+++
@@ -1,9 +0,0 @@
-package DAY_3_HT;
-
-public class exercise_13 {
-    public static void main(String[] args){
-        for (int i = 1; i <= 55; i++){
-            System.out.print(2*i - 1 + " ");
-        }
-    }
-}
